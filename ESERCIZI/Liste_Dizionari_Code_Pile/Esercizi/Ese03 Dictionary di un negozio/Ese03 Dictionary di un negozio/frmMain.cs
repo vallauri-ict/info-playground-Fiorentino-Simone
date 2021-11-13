2afcf4e1dee55f91f108b90b0fae8ee801724c71
Stud. Simone Fiorentino <s.fiorentino.1743@vallauri.net>
2021-11-13 11:02:46 +0100

    Termineted Ese03 dictonary negozio and created graphics about ese04 pronto soccorso

---
+++ ESERCIZI/Liste_Dizionari_Code_Pile/Esercizi/Ese03 Dictionary di un negozio/Ese03 Dictionary di un negozio/frmMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ese03_Dictionary_di_un_negozio
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        public struct Articolo
+        {
+            public string nome;
+            public string chiave;
+        }
+        Dictionary<int, Articolo> DictNegozio = new Dictionary<int, Articolo>();
+
+        private void btnInserisciArticolo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Articolo art;
+
+                art.nome = txtNomeArticolo.Text;
+                art.chiave = txtKeyArticolo.Text;
+                DictNegozio.Add(Convert.ToInt32(txtKeyArticolo.Text), art);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Chiave già presente, inserire un altra chiave!!");
+            }
+            
+        }
+
+        private void btnVisualizzaArticoli_Click(object sender, EventArgs e)
+        {
+            foreach (Articolo arc in DictNegozio.Values)
+            {
+                lblRisultato.Text += "\nChiave: "+arc.chiave+ "Titolo: "+arc.nome;
+            }
+        }
+
+        private void vbtnRicercaWithKey_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Articolo art = DictNegozio[Convert.ToInt32(txtKeyArticolo.Text)];
+                lblRisultato.Text = art.chiave + " " + art.nome;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Chiave inesistente");
+            }
+        }
+    }
+}
---
+++ ESERCIZI/Liste_Dizionari_Code_Pile/Esercizi/Ese04 Coda ProntoSoccorso/Ese04 Coda ProntoSoccorso/frmMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Ese04_Coda_ProntoSoccorso
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRisultato = new System.Windows.Forms.Label();
+            this.lblInserimentoRis = new System.Windows.Forms.Label();
+            this.cmbColore = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEtà = new System.Windows.Forms.TextBox();
+            this.nupTemperatura = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nupTemperatura)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblRisultato
+            // 
+            this.lblRisultato.AutoSize = true;
+            this.lblRisultato.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lblRisultato.Location = new System.Drawing.Point(28, 170);
+            this.lblRisultato.Name = "lblRisultato";
+            this.lblRisultato.Size = new System.Drawing.Size(118, 20);
+            this.lblRisultato.TabIndex = 0;
+            this.lblRisultato.Text = "RISULTATO: ";
+            // 
+            // lblInserimentoRis
+            // 
+            this.lblInserimentoRis.AutoSize = true;
+            this.lblInserimentoRis.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInserimentoRis.Location = new System.Drawing.Point(182, 170);
+            this.lblInserimentoRis.Name = "lblInserimentoRis";
+            this.lblInserimentoRis.Size = new System.Drawing.Size(0, 18);
+            this.lblInserimentoRis.TabIndex = 1;
+            // 
+            // cmbColore
+            // 
+            this.cmbColore.FormattingEnabled = true;
+            this.cmbColore.Items.AddRange(new object[] {
+            "Rosso (Urgentissimo)",
+            "Giallo (Grave)",
+            "Verde (Moderato)",
+            "Bianco (Lieve)"});
+            this.cmbColore.Location = new System.Drawing.Point(134, 77);
+            this.cmbColore.Name = "cmbColore";
+            this.cmbColore.Size = new System.Drawing.Size(121, 21);
+            this.cmbColore.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(28, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "NOME:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(134, 15);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(28, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ETA\':";
+            // 
+            // txtEtà
+            // 
+            this.txtEtà.Location = new System.Drawing.Point(134, 44);
+            this.txtEtà.Name = "txtEtà";
+            this.txtEtà.Size = new System.Drawing.Size(100, 20);
+            this.txtEtà.TabIndex = 9;
+            // 
+            // nupTemperatura
+            // 
+            this.nupTemperatura.DecimalPlaces = 2;
+            this.nupTemperatura.Increment = new decimal(new int[] {
+            20,
+            0,
+            0,
+            131072});
+            this.nupTemperatura.Location = new System.Drawing.Point(190, 124);
+            this.nupTemperatura.Maximum = new decimal(new int[] {
+            43,
+            0,
+            0,
+            0});
+            this.nupTemperatura.Minimum = new decimal(new int[] {
+            37,
+            0,
+            0,
+            0});
+            this.nupTemperatura.Name = "nupTemperatura";
+            this.nupTemperatura.Size = new System.Drawing.Size(120, 20);
+            this.nupTemperatura.TabIndex = 10;
+            this.nupTemperatura.Value = new decimal(new int[] {
+            37,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(28, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "COLORE:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "TEMPERATURA:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(454, 246);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nupTemperatura);
+            this.Controls.Add(this.txtEtà);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbColore);
+            this.Controls.Add(this.lblInserimentoRis);
+            this.Controls.Add(this.lblRisultato);
+            this.Name = "frmMain";
+            this.Text = "PRONTO SOCCORSO";
+            ((System.ComponentModel.ISupportInitialize)(this.nupTemperatura)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRisultato;
+        private System.Windows.Forms.Label lblInserimentoRis;
+        private System.Windows.Forms.ComboBox cmbColore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEtà;
+        private System.Windows.Forms.NumericUpDown nupTemperatura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
