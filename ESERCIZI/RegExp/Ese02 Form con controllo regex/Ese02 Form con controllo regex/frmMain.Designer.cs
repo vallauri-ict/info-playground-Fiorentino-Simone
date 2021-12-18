e5b67b31c64893d6efe7153be130e87d1fcfef91
Fiorentino Simone <s.fiorentino.1743@vallauri.edu>
2021-12-18 06:53:57 +0100

    Homework: ese02 regex form

--- ESERCIZI/RegExp/Ese02 Form con controllo regex/Ese02 Form con controllo regex/frmMain.Designer.cs
+++ ESERCIZI/RegExp/Ese02 Form con controllo regex/Ese02 Form con controllo regex/frmMain.Designer.cs
@@ -33,10 +33,30 @@
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.esciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.lblInizia = new System.Windows.Forms.Label();
-            this.btnAvviaForm = new System.Windows.Forms.Button();
             this.gbForm = new System.Windows.Forms.GroupBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCF = new System.Windows.Forms.TextBox();
+            this.lblCF = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.txtCittà = new System.Windows.Forms.TextBox();
+            this.lblCitta = new System.Windows.Forms.Label();
+            this.txtCAP = new System.Windows.Forms.TextBox();
+            this.lblCAP = new System.Windows.Forms.Label();
+            this.txtEtà = new System.Windows.Forms.TextBox();
+            this.lblEta = new System.Windows.Forms.Label();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.lblCognome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
             this.lblTitolo = new System.Windows.Forms.Label();
+            this.btnAvviaForm = new System.Windows.Forms.Button();
+            this.btnInvia = new System.Windows.Forms.Button();
             this.menuStripMain.SuspendLayout();
+            this.gbForm.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStripMain
@@ -79,25 +99,194 @@
             this.lblInizia.Size = new System.Drawing.Size(0, 38);
             this.lblInizia.TabIndex = 1;
             // 
-            // btnAvviaForm
-            // 
-            this.btnAvviaForm.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAvviaForm.Location = new System.Drawing.Point(732, 485);
-            this.btnAvviaForm.Name = "btnAvviaForm";
-            this.btnAvviaForm.Size = new System.Drawing.Size(218, 75);
-            this.btnAvviaForm.TabIndex = 2;
-            this.btnAvviaForm.Text = "AVVIA FORM";
-            this.btnAvviaForm.UseVisualStyleBackColor = true;
-            this.btnAvviaForm.Visible = false;
-            this.btnAvviaForm.Click += new System.EventHandler(this.avviaForm_Click);
-            // 
             // gbForm
             // 
+            this.gbForm.Controls.Add(this.btnInvia);
+            this.gbForm.Controls.Add(this.txtUserName);
+            this.gbForm.Controls.Add(this.lblUserName);
+            this.gbForm.Controls.Add(this.txtPwd);
+            this.gbForm.Controls.Add(this.label7);
+            this.gbForm.Controls.Add(this.txtCF);
+            this.gbForm.Controls.Add(this.lblCF);
+            this.gbForm.Controls.Add(this.txtMail);
+            this.gbForm.Controls.Add(this.lblMail);
+            this.gbForm.Controls.Add(this.txtCittà);
+            this.gbForm.Controls.Add(this.lblCitta);
+            this.gbForm.Controls.Add(this.txtCAP);
+            this.gbForm.Controls.Add(this.lblCAP);
+            this.gbForm.Controls.Add(this.txtEtà);
+            this.gbForm.Controls.Add(this.lblEta);
+            this.gbForm.Controls.Add(this.txtCognome);
+            this.gbForm.Controls.Add(this.lblCognome);
+            this.gbForm.Controls.Add(this.txtNome);
+            this.gbForm.Controls.Add(this.lblNome);
             this.gbForm.Location = new System.Drawing.Point(225, 97);
             this.gbForm.Name = "gbForm";
-            this.gbForm.Size = new System.Drawing.Size(501, 404);
+            this.gbForm.Size = new System.Drawing.Size(501, 451);
             this.gbForm.TabIndex = 3;
             this.gbForm.TabStop = false;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(196, 290);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(174, 20);
+            this.txtUserName.TabIndex = 17;
+            this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblUserName.Location = new System.Drawing.Point(48, 290);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(109, 22);
+            this.lblUserName.TabIndex = 16;
+            this.lblUserName.Text = "USER NAME:";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(196, 327);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(174, 20);
+            this.txtPwd.TabIndex = 15;
+            this.txtPwd.TextChanged += new System.EventHandler(this.txtPwd_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label7.Location = new System.Drawing.Point(52, 327);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 22);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "PASSWORD:";
+            // 
+            // txtCF
+            // 
+            this.txtCF.Location = new System.Drawing.Point(196, 258);
+            this.txtCF.Name = "txtCF";
+            this.txtCF.Size = new System.Drawing.Size(174, 20);
+            this.txtCF.TabIndex = 13;
+            this.txtCF.TextChanged += new System.EventHandler(this.txtCF_TextChanged);
+            // 
+            // lblCF
+            // 
+            this.lblCF.AutoSize = true;
+            this.lblCF.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblCF.Location = new System.Drawing.Point(103, 258);
+            this.lblCF.Name = "lblCF";
+            this.lblCF.Size = new System.Drawing.Size(42, 22);
+            this.lblCF.TabIndex = 12;
+            this.lblCF.Text = "C.F:";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(196, 219);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(174, 20);
+            this.txtMail.TabIndex = 11;
+            this.txtMail.TextChanged += new System.EventHandler(this.txtMail_TextChanged);
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblMail.Location = new System.Drawing.Point(34, 216);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(123, 22);
+            this.lblMail.TabIndex = 10;
+            this.lblMail.Text = "ELENCO MAIL:";
+            // 
+            // txtCittà
+            // 
+            this.txtCittà.Location = new System.Drawing.Point(196, 181);
+            this.txtCittà.Name = "txtCittà";
+            this.txtCittà.Size = new System.Drawing.Size(174, 20);
+            this.txtCittà.TabIndex = 9;
+            this.txtCittà.TextChanged += new System.EventHandler(this.txtCittà_TextChanged);
+            // 
+            // lblCitta
+            // 
+            this.lblCitta.AutoSize = true;
+            this.lblCitta.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblCitta.Location = new System.Drawing.Point(81, 181);
+            this.lblCitta.Name = "lblCitta";
+            this.lblCitta.Size = new System.Drawing.Size(72, 22);
+            this.lblCitta.TabIndex = 8;
+            this.lblCitta.Text = "CITTA\':";
+            // 
+            // txtCAP
+            // 
+            this.txtCAP.Location = new System.Drawing.Point(196, 142);
+            this.txtCAP.Name = "txtCAP";
+            this.txtCAP.Size = new System.Drawing.Size(174, 20);
+            this.txtCAP.TabIndex = 7;
+            this.txtCAP.TextChanged += new System.EventHandler(this.txtCAP_TextChanged);
+            // 
+            // lblCAP
+            // 
+            this.lblCAP.AutoSize = true;
+            this.lblCAP.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblCAP.Location = new System.Drawing.Point(108, 142);
+            this.lblCAP.Name = "lblCAP";
+            this.lblCAP.Size = new System.Drawing.Size(45, 22);
+            this.lblCAP.TabIndex = 6;
+            this.lblCAP.Text = "CAP:";
+            // 
+            // txtEtà
+            // 
+            this.txtEtà.Location = new System.Drawing.Point(196, 106);
+            this.txtEtà.Name = "txtEtà";
+            this.txtEtà.Size = new System.Drawing.Size(174, 20);
+            this.txtEtà.TabIndex = 5;
+            this.txtEtà.TextChanged += new System.EventHandler(this.txtEtà_TextChanged);
+            // 
+            // lblEta
+            // 
+            this.lblEta.AutoSize = true;
+            this.lblEta.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblEta.Location = new System.Drawing.Point(99, 106);
+            this.lblEta.Name = "lblEta";
+            this.lblEta.Size = new System.Drawing.Size(54, 22);
+            this.lblEta.TabIndex = 4;
+            this.lblEta.Text = "ETA\':";
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(196, 73);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(174, 20);
+            this.txtCognome.TabIndex = 3;
+            this.txtCognome.TextChanged += new System.EventHandler(this.txtCognome_TextChanged);
+            // 
+            // lblCognome
+            // 
+            this.lblCognome.AutoSize = true;
+            this.lblCognome.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblCognome.Location = new System.Drawing.Point(63, 73);
+            this.lblCognome.Name = "lblCognome";
+            this.lblCognome.Size = new System.Drawing.Size(94, 22);
+            this.lblCognome.TabIndex = 2;
+            this.lblCognome.Text = "COGNOME:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(196, 40);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(174, 20);
+            this.txtNome.TabIndex = 1;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblNome.Location = new System.Drawing.Point(94, 40);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(63, 22);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "NOME:";
             // 
             // lblTitolo
             // 
@@ -109,14 +298,36 @@
             this.lblTitolo.TabIndex = 0;
             this.lblTitolo.Text = "FORM CON REGEX";
             // 
+            // btnAvviaForm
+            // 
+            this.btnAvviaForm.Font = new System.Drawing.Font("Comic Sans MS", 15.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnAvviaForm.Location = new System.Drawing.Point(743, 248);
+            this.btnAvviaForm.Name = "btnAvviaForm";
+            this.btnAvviaForm.Size = new System.Drawing.Size(207, 76);
+            this.btnAvviaForm.TabIndex = 0;
+            this.btnAvviaForm.Text = "AVVIA FORM";
+            this.btnAvviaForm.UseVisualStyleBackColor = true;
+            this.btnAvviaForm.Click += new System.EventHandler(this.avviaForm_Click);
+            // 
+            // btnInvia
+            // 
+            this.btnInvia.Font = new System.Drawing.Font("Comic Sans MS", 15.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnInvia.Location = new System.Drawing.Point(147, 375);
+            this.btnInvia.Name = "btnInvia";
+            this.btnInvia.Size = new System.Drawing.Size(204, 51);
+            this.btnInvia.TabIndex = 18;
+            this.btnInvia.Text = "INVIA FORM";
+            this.btnInvia.UseVisualStyleBackColor = true;
+            this.btnInvia.Click += new System.EventHandler(this.btnInvia_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(950, 560);
+            this.Controls.Add(this.btnAvviaForm);
             this.Controls.Add(this.lblTitolo);
             this.Controls.Add(this.gbForm);
-            this.Controls.Add(this.btnAvviaForm);
             this.Controls.Add(this.lblInizia);
             this.Controls.Add(this.menuStripMain);
             this.MainMenuStrip = this.menuStripMain;
@@ -125,6 +336,8 @@
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.menuStripMain.ResumeLayout(false);
             this.menuStripMain.PerformLayout();
+            this.gbForm.ResumeLayout(false);
+            this.gbForm.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,9 +350,28 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem esciToolStripMenuItem;
         private System.Windows.Forms.Label lblInizia;
-        private System.Windows.Forms.Button btnAvviaForm;
         private System.Windows.Forms.GroupBox gbForm;
         private System.Windows.Forms.Label lblTitolo;
+        private System.Windows.Forms.Button btnAvviaForm;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCF;
+        private System.Windows.Forms.Label lblCF;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.TextBox txtCittà;
+        private System.Windows.Forms.Label lblCitta;
+        private System.Windows.Forms.TextBox txtCAP;
+        private System.Windows.Forms.Label lblCAP;
+        private System.Windows.Forms.TextBox txtEtà;
+        private System.Windows.Forms.Label lblEta;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.Label lblCognome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Button btnInvia;
     }
 }
 
--- ESERCIZI/RegExp/Ese02 Form con controllo regex/Ese02 Form con controllo regex/frmMain.cs
+++ ESERCIZI/RegExp/Ese02 Form con controllo regex/Ese02 Form con controllo regex/frmMain.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Ese02_Form_con_controllo_regex
@@ -16,16 +17,206 @@ namespace Ese02_Form_con_controllo_regex
         {
             InitializeComponent();
         }
+        bool[] corretti = new bool[9];
 
         private void frmMain_Load(object sender, EventArgs e)
         {
             lblInizia.Text = "AVVIA LA FORM, premendo il pulsante qui sotto";
+            lblTitolo.Visible = false;
+            gbForm.Visible = false;
+            btnAvviaForm.Location = new Point (300, 210);
+            btnAvviaForm.Visible = true;
         }
 
         private void avviaForm_Click(object sender, EventArgs e)
         {
             lblInizia.Visible = false;
             btnAvviaForm.Visible = false;
+
+
+            /*VISIBLE: */
+            lblTitolo.Visible = true;
+            gbForm.Visible = true;
+        }
+
+
+        /* FUNCTIONS TEXT CHANGED */
+        private void txtNome_TextChanged(object sender, EventArgs e) //CORRETTA
+        {
+            string content = txtNome.Text;
+            Regex rgx = new Regex(@"^([a-zA-Z]{3,})$");
+            if (rgx.IsMatch(content))
+            {
+                txtNome.BackColor = Color.Green;
+                corretti[0] = true;
+            }
+            else
+            {
+                txtNome.BackColor = Color.Red;
+                corretti[0] = false;
+            }
+        }
+
+        private void txtCognome_TextChanged(object sender, EventArgs e) //CORRETTA
+        {
+            string content = txtCognome.Text;
+            Regex rgx = new Regex(@"^([a-zA-Z]{3,})$");
+            if (rgx.IsMatch(content))
+            {
+                txtCognome.BackColor = Color.Green;
+                corretti[1] = true;
+            }
+            else
+            {
+                txtCognome.BackColor = Color.Red;
+                corretti[1] = false;
+            }
+        }
+
+        private void txtEtà_TextChanged(object sender, EventArgs e) //NON CORRETTA, sicome va fino a 100 massimo
+        {
+            string content = txtEtà.Text;
+            Regex rgx = new Regex(@"^([0-9]|[1-9][0-9]|100)$");
+            if (rgx.IsMatch(content))
+            {
+                txtEtà.BackColor = Color.Green;
+                corretti[2] = true;
+            }
+            else
+            {
+                txtEtà.BackColor = Color.Red;
+                corretti[2] = false;
+            }
+        }
+
+        private void txtCAP_TextChanged(object sender, EventArgs e) //CORRETTA
+        {
+            string content = txtCAP.Text;
+            Regex rgx = new Regex(@"^\d{5}$");
+            if (rgx.IsMatch(content))
+            {
+                txtCAP.BackColor = Color.Green;
+                corretti[3] = true;
+            }
+            else
+            {
+                txtCAP.BackColor = Color.Red;
+                corretti[3] = false;
+            }
+        }
+
+        private void txtCittà_TextChanged(object sender, EventArgs e) //CORRETTA
+        {
+            string content = txtCittà.Text;
+            Regex rgx = new Regex(@"^[a-zA-Z]{3,} [a-zA-Z]{2}$");
+            if (rgx.IsMatch(content))
+            {
+                txtCittà.BackColor = Color.Green;
+                corretti[4] = true;
+            }
+            else
+            {
+                txtCittà.BackColor = Color.Red;
+                corretti[4] = false;
+            }
+        }
+
+        private void txtMail_TextChanged(object sender, EventArgs e) //CORRETTA
+        {
+            string content = txtMail.Text;
+            Regex rgx = new Regex(@"^(\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)(,\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)*$");
+            if (rgx.IsMatch(content))
+            {
+                txtMail.BackColor = Color.Green;
+                corretti[5] = true;
+            }
+            else
+            {
+                txtMail.BackColor = Color.Red;
+                corretti[5] = false;
+            }
+        }
+       
+
+        private void txtCF_TextChanged(object sender, EventArgs e) //CORRETTA
+        {
+            string content = txtCF.Text;
+            Regex rgx = new Regex(@"^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$");
+            if (rgx.IsMatch(content))
+            {
+                txtCF.BackColor = Color.Green;
+                corretti[6] = true;
+            }
+            else
+            {
+                txtCF.BackColor = Color.Red;
+                corretti[6] = false;
+            }
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e) //CORRETTA
+        {
+            string content = txtUserName.Text;
+            Regex rgx = new Regex(@"[a-zA-Z]\w{4,}");
+            if (rgx.IsMatch(content))
+            {
+                txtUserName.BackColor = Color.Green;
+                corretti[7] = true;
+            }
+            else
+            {
+                txtUserName.BackColor = Color.Red;
+                corretti[7] = false;
+            }
+        }
+
+        private void txtPwd_TextChanged(object sender, EventArgs e) //NON CORRETTA
+        {
+            string content = txtPwd.Text;
+            Regex rgx = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[!-+])([A-Za-z\d!-+]{1,}){8,15}$");
+            if (rgx.IsMatch(content))
+            {
+                txtPwd.BackColor = Color.Green;
+                corretti[8] = true;
+            }
+            else
+            {
+                txtPwd.BackColor = Color.Red;
+                corretti[8] = false;
+            }
+        }
+
+        private void btnInvia_Click(object sender, EventArgs e)
+        {
+            bool procedi = true;
+            int sbagliate = 0;
+            string msg;
+            for (int i = 0; i < corretti.Length; i++)
+            {
+                if (corretti[i] == false)
+                {
+                    procedi = false;
+                    ++sbagliate;
+                }
+            }
+            if (procedi)
+            {
+                msg = "Invio del form correttamente!!\nElenco mail:\n";
+                if (txtMail.Text.Contains(","))
+                {
+                    for (int i = 0; i < txtMail.Text.Split(',').Length; i++)
+                    {
+                        if (i == 0)
+                            msg += " ";
+                        msg += txtMail.Text.Split(',')[i] + "\n";
+                    }
+                }
+            }
+            else
+            {
+                msg = "Ci sono " + sbagliate + " errori, non si può inviare il form!";
+            }
+            MessageBox.Show(msg,"Invio del Form",MessageBoxButtons.OK);
         }
     }
 }
